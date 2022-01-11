d47eae39010973b79ad6fb7f1a8d79af327eeb37
Fran <fjnf@outlook.com>
2022-01-11 16:20:33 -0300

    leetcode easy array3 rotate

---
+++ 01-arrays/02-maxProfit.py
@@ -0,0 +1,24 @@
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+# Find and return the maximum profit you can achieve.
+
+from typing import List
+
+
+class Solution:
+    def testExpectedNums():
+        prices = [7, 1, 5, 3, 6, 4]
+        expectedReturn = 7
+        assert expectedReturn == Solution.maxProfit(
+            Solution, prices), "Values should be equal."
+
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            max_profit += max(prices[i+1] - prices[i], 0)
+        return max_profit
+
+
+if __name__ == "__main__":
+    Solution.testExpectedNums()
+    print("OK. Values are equal.")
