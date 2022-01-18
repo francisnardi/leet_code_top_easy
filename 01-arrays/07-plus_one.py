04fbe083a2d7f5f4c0c49b6bf828726bac2b4648
Fran <fjnf@outlook.com>
2022-01-18 00:02:04 -0300

    leetcode easy array7 move_zeroes

---
+++ 01-arrays/07-plus_one.py
@@ -0,0 +1,32 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+# The digits are ordered from most significant to least significant in left-to-right order.
+# The large integer does not contain any leading 0's.
+# Increment the large integer by one and return the resulting array of digits.
+
+
+from typing import List
+import collections
+
+
+class Solution:
+    def test_expected_nums(self) -> None:
+        nums = [0, 1, 0, 3, 12]
+        expected_nums = [1, 3, 12, 0, 0]
+
+        assert collections.Counter(expected_nums) == collections.Counter(
+            Solution.move_zeroes(self, nums)), "Lists should be equal."
+
+    def move_zeroes(self, nums: List[int]) -> List[int]:
+        index = 0
+        for item in range(len(nums)):
+            if nums[item] != 0:
+                temp = nums[item]
+                nums[item] = nums[index]
+                nums[index] = temp
+                index += 1
+        return nums
+
+
+if __name__ == "__main__":
+    Solution.test_expected_nums(Solution)
+    print("OK. Values are equal.")
